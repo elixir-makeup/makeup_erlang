b02914f3a96dac8f302faab476b9e2093008388f
Wojtek Mach <wojtek@wojtekmach.pl>
2021-03-23 14:32:26 +0100

    Remove unused assert_value dependency

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,4 @@
 # Used by "mix format"
 [
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  # don't add parens around assert_value arguments
-  import_deps: [:assert_value]
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
