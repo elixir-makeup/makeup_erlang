f9baf2cfc36dc0709b72228e8cfecc94dfdb82ed
srfsh <dev@srf.sh>
2022-11-08 16:03:23 +0300

    Bump required Elixir version to 1.10

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule MakeupErlang.Mixfile do
     [
       app: :makeup_erlang,
       version: @version,
-      elixir: "~> 1.6",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
